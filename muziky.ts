abddcae4ffbe60d1f40610999b40af286841543b
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-10-08 14:43:33 -0400

    Update muziky.ts

--- muziky.ts
+++ muziky.ts
@@ -1,1 +1,8 @@
-import 
+import { Application } from "@sveltejs/kit";
+import { createServer } from "@sveltejs/kit/server";
+
+import { handle } from "./routes/api/+server";
+
+const app = new Application();
+
+app.use(handle);
