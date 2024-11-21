bfd4d453dc2931cc3a8498780625c8e87f15418a
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-11-21 15:46:17 -0500

    Add files via upload

---
+++ app/src/explore/[slug]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+ import Carousel from "$lib/components/Carousel/Carousel.svelte";
+ import { Grid } from "$lib/components/Grid";
+ import Header from "$lib/components/Header.svelte";
+ import CarouselItem from "$lib/components/Carousel/CarouselItem.svelte";
+ import type { PageData } from "./$types";
+ 
+ export let data: PageData;
+
+ 
+ let sections = data?.sections;
+ let path = data?.path;
+
+</script>
