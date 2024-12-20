8a360379b1212552a9591b07afc233148dadc7c1
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-20 13:33:44 -0500

    Add files via upload

---
+++ equalizerpresets.ts
@@ -0,0 +1,21 @@
+// Equalizer presets
+const equalizerPresets = {
+    Flat: [0, 0, 0, 0, 0],
+    Pop: [2, 4, 0, 2, 1],
+    Rock: [4, 2, -2, 2, 4],
+    Jazz: [3, 1, 0, 3, 2],
+    Classical: [3, 2, 0, 2, 3],
+    Dance: [1, 1, 1, 1, 1],
+    Party: [4, 2, -2, 2, 4],
+    Soft: [0, 1, -1, 1, 0],
+    Hard: [2, 2, -2, 2, 2],
+    TrebleBoost: [4, 2, 0, -2, 4],
+    MidBoost: [2, 4, 0, -2, 2],
+    Stereo: [0, 0, 0, 0, 0],
+    Mono: [0, 0, 0, 0, 0],
+    SubBass: [1, 1, 1, 1, 1],
+    BassBoost: [5, 3, 0, -2, -4],
+  };
+  
+  const frequencies = [60, 170, 350, 1000, 3500]; // Example frequency bands
+  
